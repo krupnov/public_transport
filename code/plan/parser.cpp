52cf3d2ec8892e7181f47d4174df36aa9b6fe76b
Ilia Krupnov <krupnov.iv@gmail.com>
2019-02-17 09:57:39 +0100

    Routes parsing added

--- code/plan/parser.cpp
+++ code/plan/parser.cpp
@@ -6,23 +6,51 @@
 
 #include <exception>
 #include <unordered_map>
+#include <iostream>
 
 namespace fs = boost::filesystem;
+namespace ds = data_structures;
 
 namespace {
-    constexpr int AGENCIES_COLUMN_COUNT = 4;
+    template<int column_count>
+    using csv_reader = io::CSVReader<column_count, io::trim_chars<' '>, io::double_quote_escape<',','\"'>>;
 
-    std::unordered_map<std::string, std::shared_ptr<data_structures::agency_t>> parse_agencies(fs::path const& path) {
-        std::unordered_map<std::string, std::shared_ptr<data_structures::agency_t>> agencies;
-        io::CSVReader<AGENCIES_COLUMN_COUNT, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> reader(
-                path.string());
+    constexpr int AGENCIES_COLUMN_COUNT = 4;
+    constexpr int ROUTES_COLUMN_COUNT = 6;
+
+    ds::value_by_id<ds::agency_ptr> parse_agencies(fs::path const& path) {
+        ds::value_by_id<ds::agency_ptr> agencies;
+        csv_reader<AGENCIES_COLUMN_COUNT> reader(path.string());
         reader.read_header(io::ignore_extra_column, "agency_id", "agency_name", "agency_url", "agency_timezone");
-        auto agency = std::make_shared<data_structures::agency_t>();
+        auto agency = std::make_shared<ds::agency_t>();
         while (reader.read_row(agency->id, agency->name, agency->url, agency->timezone)) {
             agencies.emplace(agency->id,  std::move(agency));
-            agency = std::make_shared<data_structures::agency_t>();
+            agency = std::make_shared<ds::agency_t>();
         }
         return agencies;
+    }
+
+    fs::path get_table_path(fs::path const& directory, fs::path const& file) {
+        if (!fs::is_regular_file(directory / file)) {
+            throw std::runtime_error("No table found: " + file.string());
+        }
+        return directory / file;
+    }
+
+    ds::value_by_id<ds::route_ptr> parse_routes(fs::path const& path, ds::value_by_id<ds::agency_ptr>& agencies) {
+        ds::value_by_id<ds::route_ptr> routes;
+        csv_reader<ROUTES_COLUMN_COUNT> reader(path.string());
+        reader.read_header(io::ignore_extra_column,
+                "route_id", "agency_id", "route_short_name", "route_long_name", "route_desc" ,"route_type");
+        auto route = std::make_shared<ds::route_t>();
+        std::string agency_id;
+        while (reader.read_row(route->id, agency_id, route->short_name, route->long_name, route->desc, route->type)) {
+            route->agency = agencies.at(agency_id);
+            agencies.at(agency_id)->routes.push_back(route);
+            routes.emplace(route->id, std::move(route));
+            route = std::make_shared<ds::route_t>();
+        }
+        return routes;
     }
 }
 
@@ -32,10 +60,9 @@ namespace util {
             throw std::runtime_error("Feed directory is not directory: " + feed_directory);
         }
         fs::path feed(feed_directory);
-        fs::path agency_file("agency.txt");
-        if (!fs::is_regular_file(feed_directory / agency_file)) {
-            throw std::runtime_error("No agency.txt file found");
-        }
-        auto agencies = parse_agencies(feed_directory / agency_file);
+        auto agencies = parse_agencies(get_table_path(feed, "agency.txt"));
+        std::cout << "Agencies count: " << agencies.size() << std::endl;
+        auto routes = parse_routes(get_table_path(feed, "routes.txt"), agencies);
+        std::cout << "Routes count: " << routes.size() << std::endl;
     }
 }
--- code/plan/structures.h
+++ code/plan/structures.h
@@ -2,14 +2,37 @@
 #define PLANNER_STRUCTURES_H
 
 #include <boost/cstdint.hpp>
+#include <memory>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 namespace data_structures {
+    struct route_t;
+    struct agency_t;
+
+
+    template<typename T>
+    using value_by_id = std::unordered_map<std::string, T>;
+
+    using route_ptr = std::shared_ptr<route_t>;
+    using agency_ptr = std::shared_ptr<agency_t>;
+
     struct agency_t {
         std::string id;
         std::string name;
         std::string url;
         std::string timezone;
+        std::vector<route_ptr> routes;
+    };
+
+    struct route_t {
+        std::string id;
+        agency_ptr agency;
+        std::string short_name;
+        std::string long_name;
+        std::string desc;
+        int type;
     };
 }
 
